1ee6a2479275af7bd4e8d8147dfeca886b5b2422
wyozi <joonas.mertanen@gmail.com>
2020-03-10 13:38:04 +0800

    Build a proper tester link

--- index.ts
+++ index.ts
@@ -6,6 +6,11 @@ const fs = require("fs");
 const readFile = util.promisify(fs.readFile);
 const META_REGEX_PATTERN = /\/([^\/\n]+)\//g;
 
+const buildTesterUrl = (pattern: string) => {
+  const encoded = encodeURIComponent(pattern);
+  return `https://pythex.org/?regex=${encoded}&test_string=&ignorecase=0&multiline=0&dotall=0&verbose=0`
+}
+
 async function run() {
   
   const issue: { owner: string; repo: string; number: number } =
@@ -33,13 +38,20 @@ async function run() {
     return matches?.length > 0 ? [path, matches] as [string, RegExpMatchArray] : null;
   }).filter(x => x !== null);
 
+  const body = `## Found Regex Patterns
+  ${fileRegexps.map(([path, matches]) => {
+    return `### ${path}\n${
+      matches.map(match =>
+        `*${match}* ${buildTesterUrl(match)}`
+      ).join("\n")
+    }`;
+  }).join("\n\n")}`;
+
   await client.issues.createComment({
     owner: issue.owner,
     repo: issue.repo,
     issue_number: issue.number,
-    body: fileRegexps.map(([path, matches]) => {
-      return `${path}: ${matches.join(", ")}`;
-    }).join("\n")
+    body
   });
 }
 
